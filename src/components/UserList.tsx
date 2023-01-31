a335e391fe3b4050e814fd3e83e38a56df7a9672
Ash-exp <asutosh2000ad@gmail.com>
2023-01-31 11:20:21 +0530

    ui update 2.0

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -6,11 +6,11 @@ const UserList = () => {
   let { userList, removeUser } = useUserContext();
   return (
     <section className="row justify-content-center mt-2 min-vh-100">
-      <div className="col-auto my-auto">
-        <table className="table align-middle table-responsive">
+      <div className="col-auto my-auto table-responsive">
+        <table className="table align-middle">
           <thead className="thead-dark bg-dark text-white">
             <tr>
-              <th scope="col">Sr</th>
+              <th scope="col ">Sr</th>
               <th scope="col">Username</th>
               <th scope="col">mobileNumber</th>
               <th scope="col">emailId</th>
@@ -30,7 +30,7 @@ const UserList = () => {
             )}
             {userList.map((User, index) => {
               return (
-                <tr key={index}>
+                <tr key={index} className="text-wrap">
                   <th scope="row">{index + 1}</th>
                   <td>{User.userName}</td>
                   <td>{User.mobileNumber}</td>
@@ -39,20 +39,22 @@ const UserList = () => {
                   <td>{User.DOB}</td>
                   <td>{User.gender}</td>
                   <td>
-                    <button
-                      className="btn btn-sm btn-danger mx-1"
-                      title="delete"
-                      onClick={() => removeUser(index)}
-                    >
-                      <i className="fa fa-trash" aria-hidden="true"></i>
-                    </button>
-                    <button
-                      className="btn btn-sm btn-primary mx-1"
-                      title="edit"
-                      onClick={() => navigate('/user/edit/' + User.id)}
-                    >
-                      <i className="fa fa-edit" aria-hidden="true"></i>
-                    </button>
+                    <div className="btn-group">
+                      <button
+                        className="btn btn-sm btn-danger ml-1"
+                        title="delete"
+                        onClick={() => removeUser(index)}
+                      >
+                        <i className="fa fa-trash" aria-hidden="true"></i>
+                      </button>
+                      <button
+                        className="btn btn-sm btn-primary mr-1"
+                        title="edit"
+                        onClick={() => navigate('/user/edit/' + User.id)}
+                      >
+                        <i className="fa fa-edit" aria-hidden="true"></i>
+                      </button>
+                    </div>
                   </td>
                 </tr>
               );
